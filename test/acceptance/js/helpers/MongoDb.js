d39a4f8d12e3825585fafbb6a20816eac30c4b2e
Eric Mc Sween <eric.mcsween@overleaf.com>
2019-07-05 10:53:43 -0400

    Prettier: convert test/acceptance decaffeinated files to Prettier format

--- test/acceptance/js/helpers/MongoDb.js
+++ test/acceptance/js/helpers/MongoDb.js
@@ -9,82 +9,102 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const async = require("async");
-const bcrypt = require("bcrypt");
-const mongojs = require('mongojs');
-const logger = require("logger-sharelatex");
-const Settings = require("settings-sharelatex");
+const async = require('async')
+const bcrypt = require('bcrypt')
+const mongojs = require('mongojs')
+const logger = require('logger-sharelatex')
+const Settings = require('settings-sharelatex')
 
 module.exports = {
-	running: false,
-	initing: false,
-	callbacks: [],
-	db: mongojs(Settings.mongo.url, ['users', 'projects']),
+  running: false,
+  initing: false,
+  callbacks: [],
+  db: mongojs(Settings.mongo.url, ['users', 'projects']),
 
-	ensureRunning(callback) {
-		if (this.running) {
-			return callback();
-		}
-		if (this.initing) {
-			this.callbacks.push(callback);
-			return;
-		}
-		this.initing = true;
-		this.callbacks.push(callback);
-		// Force a connection by executing a command
-		return this.db.stats(err => {
-			if (!err) {
-				this.running = true;
-				logger.info("MongoDB ready");
-			}
-			return (() => {
-				const result = [];
-				for (callback of Array.from(this.callbacks)) {
-					result.push(callback(err));
-				}
-				return result;
-			})();
-		});
-	},
+  ensureRunning(callback) {
+    if (this.running) {
+      return callback()
+    }
+    if (this.initing) {
+      this.callbacks.push(callback)
+      return
+    }
+    this.initing = true
+    this.callbacks.push(callback)
+    // Force a connection by executing a command
+    return this.db.stats(err => {
+      if (!err) {
+        this.running = true
+        logger.info('MongoDB ready')
+      }
+      return (() => {
+        const result = []
+        for (callback of Array.from(this.callbacks)) {
+          result.push(callback(err))
+        }
+        return result
+      })()
+    })
+  },
 
-	createDummyUser(params, callback) {
-		const { email, password, numProjects } = params;
-		return async.auto({
-			hashedPassword: cb => {
-				return bcrypt.hash(password, 10, cb);
-			},
-			user: ['hashedPassword', ({ hashedPassword }, cb) => {
-				return this.db.users.insert({
-					email,
-					hashedPassword
-				}, cb);
-			}
-			],
-			projects: ['user', ({ user }, cb) => {
-				if (numProjects === 0) {
-					return cb(null, []);
-				}
-				const projects = [];
-				for (let i = 0, end = numProjects, asc = end >= 0; asc ? i < end : i > end; asc ? i++ : i--) {
-					projects.push({
-						name: `Project ${i + 1}`,
-						owner_ref: user._id,
-						collaberator_refs: [],
-						lastUpdated: new Date(),
-						readOnly_refs: []
-					});
-				}
-				return this.db.projects.insert(projects, cb);
-			}
-			]
-		}, (err, { user, projects }) => {
-			if (err != null) {
-				return callback(err);
-			}
-			return callback(null, Object.assign({
-				password,
-				projects
-			}, user));
-		});
-	}
-};
+  createDummyUser(params, callback) {
+    const { email, password, numProjects } = params
+    return async.auto(
+      {
+        hashedPassword: cb => {
+          return bcrypt.hash(password, 10, cb)
+        },
+        user: [
+          'hashedPassword',
+          ({ hashedPassword }, cb) => {
+            return this.db.users.insert(
+              {
+                email,
+                hashedPassword
+              },
+              cb
+            )
+          }
+        ],
+        projects: [
+          'user',
+          ({ user }, cb) => {
+            if (numProjects === 0) {
+              return cb(null, [])
+            }
+            const projects = []
+            for (
+              let i = 0, end = numProjects, asc = end >= 0;
+              asc ? i < end : i > end;
+              asc ? i++ : i--
+            ) {
+              projects.push({
+                name: `Project ${i + 1}`,
+                owner_ref: user._id,
+                collaberator_refs: [],
+                lastUpdated: new Date(),
+                readOnly_refs: []
+              })
+            }
+            return this.db.projects.insert(projects, cb)
+          }
+        ]
+      },
+      (err, { user, projects }) => {
+        if (err != null) {
+          return callback(err)
+        }
+        return callback(
+          null,
+          Object.assign(
+            {
+              password,
+              projects
+            },
+            user
+          )
+        )
+      }
+    )
+  }
+}
--- test/acceptance/js/helpers/ReadOnlyApp.js
+++ test/acceptance/js/helpers/ReadOnlyApp.js
@@ -10,38 +10,38 @@
  * DS205: Consider reworking code to avoid use of IIFEs
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const app = require('../../../../app');
-require("logger-sharelatex").logger.level("info");
-const logger = require("logger-sharelatex");
-const Settings = require("settings-sharelatex");
+const app = require('../../../../app')
+require('logger-sharelatex').logger.level('info')
+const logger = require('logger-sharelatex')
+const Settings = require('settings-sharelatex')
 
 module.exports = {
-	running: false,
-	initing: false,
-	callbacks: [],
+  running: false,
+  initing: false,
+  callbacks: [],
 
-	ensureRunning(callback) {
-		if (this.running) {
-			return callback();
-		}
-		if (this.initing) {
-			this.callbacks.push(callback);
-			return;
-		}
-		this.initing = true;
-		this.callbacks.push(callback);
-		return app.listen(3038, err => {
-			if (!err) {
-				this.running = true;
-				logger.info("read-only running in dev mode");
-			}
-			return (() => {
-				const result = [];
-				for (callback of Array.from(this.callbacks)) {
-					result.push(callback(err));
-				}
-				return result;
-			})();
-		});
-	}
-};
+  ensureRunning(callback) {
+    if (this.running) {
+      return callback()
+    }
+    if (this.initing) {
+      this.callbacks.push(callback)
+      return
+    }
+    this.initing = true
+    this.callbacks.push(callback)
+    return app.listen(3038, err => {
+      if (!err) {
+        this.running = true
+        logger.info('read-only running in dev mode')
+      }
+      return (() => {
+        const result = []
+        for (callback of Array.from(this.callbacks)) {
+          result.push(callback(err))
+        }
+        return result
+      })()
+    })
+  }
+}
