60462aed8f3dd7dfd67c37a9c28df318ec988803
decaffeinate <eric.mcsween@overleaf.com>
2019-07-05 10:54:23 -0400

    decaffeinate: Convert app.coffee to JS

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
-async = require "async"
-Settings = require "settings-sharelatex"
-logger = require "logger-sharelatex"
-express = require "express"
-Metrics = require "metrics-sharelatex"
-Path = require "path"
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const async = require("async");
+const Settings = require("settings-sharelatex");
+const logger = require("logger-sharelatex");
+const express = require("express");
+const Metrics = require("metrics-sharelatex");
+const Path = require("path");
 
-EmailSender = require "./app/js/EmailSender"
-MongoHandler = require "./app/js/MongoHandler"
-Router = require "./app/js/Router"
+const EmailSender = require("./app/js/EmailSender");
+const MongoHandler = require("./app/js/MongoHandler");
+const Router = require("./app/js/Router");
 
-Metrics.initialize("read-only")
-logger.initialize("read-only")
-EmailSender.initialize()
+Metrics.initialize("read-only");
+logger.initialize("read-only");
+EmailSender.initialize();
 
-app = express()
-app.set('views', __dirname + '/app/views')
-app.set('view engine', 'pug')
-Router.initialize(app)
+const app = express();
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'pug');
+Router.initialize(app);
 
-port = Settings.internal.read_only.port
-host = Settings.internal.read_only.host
-if !module.parent # Called directly
-	async.series {
-		initDb: (cb) ->
-			MongoHandler.initialize(cb)
-		startHttpServer: (cb) ->
-			logger.info("Starting HTTP server")
-			app.listen(port, host, cb)
-	}, (err) ->
-		if err?
-			throw err
-		logger.info("HTTP server ready and listening on #{host}:#{port}")
+const { port } = Settings.internal.read_only;
+const { host } = Settings.internal.read_only;
+if (!module.parent) { // Called directly
+	async.series({
+		initDb(cb) {
+			return MongoHandler.initialize(cb);
+		},
+		startHttpServer(cb) {
+			logger.info("Starting HTTP server");
+			return app.listen(port, host, cb);
+		}
+	}, function(err) {
+		if (err != null) {
+			throw err;
+		}
+		return logger.info(`HTTP server ready and listening on ${host}:${port}`);
+	});
+}
 
-module.exports = app
+module.exports = app;
