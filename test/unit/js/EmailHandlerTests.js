9f32e5a2c8b503a0039151ff229a0f0efa900929
Eric Mc Sween <eric.mcsween@overleaf.com>
2019-07-12 12:31:13 -0400

    Fix unit tests following mongojs removal
    I forgot to grep the unit tests when removing mongojs.

--- test/unit/js/EmailHandlerTests.js
+++ test/unit/js/EmailHandlerTests.js
@@ -1,21 +1,7 @@
-/* eslint-disable
-    no-return-assign,
-    no-undef,
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const path = require('path')
 const sinon = require('sinon')
 const { expect } = require('chai')
 const SandboxedModule = require('sandboxed-module')
-const { ObjectId } = require('mongojs')
 
 const MODULE_PATH = path.join(__dirname, '../../../app/js/EmailHandler.js')
 
@@ -33,20 +19,20 @@ describe('EmailHandler', function() {
     this.EmailSender = {
       sendMail: sinon.stub().yields()
     }
-    return (this.EmailHandler = SandboxedModule.require(MODULE_PATH, {
+    this.EmailHandler = SandboxedModule.require(MODULE_PATH, {
       requires: {
         'settings-sharelatex': this.Settings,
         'logger-sharelatex': this.logger,
         './EmailSender': this.EmailSender
       }
-    }))
+    })
   })
 
-  return describe('sendOneTimeLoginEmail', () =>
+  describe('sendOneTimeLoginEmail', () =>
     it('builds and sends an email', function(done) {
       const email = 'user@example.com'
       const token = 'secret123'
-      return this.EmailHandler.sendOneTimeLoginEmail(email, token, err => {
+      this.EmailHandler.sendOneTimeLoginEmail(email, token, err => {
         if (err != null) {
           return done(err)
         }
@@ -58,7 +44,7 @@ describe('EmailHandler', function() {
           text: sinon.match(/secret123/),
           html: sinon.match(/secret123/)
         })
-        return done()
+        done()
       })
     }))
 })
